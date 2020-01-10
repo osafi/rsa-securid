3b3134ab4b9d0fd70031dd68c7f0bf8692570f22
Alpha Chen <alpha@kejadlen.dev>
2020-01-09 17:18:19 -0800

    it works

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,48 +1,107 @@
 extern crate chrono;
+extern crate openssl;
+
+use std::env;
 
 use chrono::prelude::*;
+use openssl::symm::{encrypt, Cipher};
 
-struct Token {
-    length: usize,
-    seed: String,
-    serial: String,
-}
+const FLD_DIGIT_SHIFT: u8 = 6;
+const FLD_DIGIT_MASK: u16 = (0b111 << FLD_DIGIT_SHIFT);
 
 fn main() {
-    let time = Utc::now();
+    let serial = env::var("RSA_AUTH_SERIAL").unwrap();
+    let seed: Vec<u8> = env::var("RSA_AUTH_SEED").unwrap().split(':').map(|x| u8::from_str_radix(x, 16).unwrap()).collect();
 
+    let time = Utc::now();
     let bcd_time = bcd_time(time);
 
-    dbg!(bcd_time);
+    let mut key0 = [0; 16];
+    let mut key1 = [0; 16];
 
-    println!("Hello, world!");
+    key_from_time(bcd_time, 2, &serial, &mut key0);
+    aes128_ecb_encrypt(&mut key0, seed.as_slice());
+
+    key_from_time(bcd_time, 3, &serial, &mut key1);
+    aes128_ecb_encrypt(&mut key1, &key0);
+
+    key_from_time(bcd_time, 4, &serial, &mut key0);
+    aes128_ecb_encrypt(&mut key0, &key1);
+
+    key_from_time(bcd_time, 5, &serial, &mut key1);
+    aes128_ecb_encrypt(&mut key1, &key0);
+
+    key_from_time(bcd_time, 8, &serial, &mut key0);
+    aes128_ecb_encrypt(&mut key0, &key1);
+
+    let mut i = ((time.minute() as usize) & 0b11) << 2;
+    let t1 = ((key0[i + 0] as u32) & 0xff) << 24;
+    let t2 = ((key0[i + 1] as u32) & 0xff) << 16;
+    let t3 = ((key0[i + 2] as u32) & 0xff) << 8;
+    let t4 = ((key0[i + 3] as u32) & 0xff) << 0;
+    let mut token_code = t1 | t2 | t3 | t4;
+
+    let mut out = vec![];
+    let mut j: isize = ((17369 & FLD_DIGIT_MASK) >> FLD_DIGIT_SHIFT) as isize;
+    while j >= 0 {
+        let mut c = (token_code % 10) as u8;
+        token_code /= 10;
+
+        if i < 8 {
+            c += 0;
+        }
+        out.push(c % 10);
+
+        j -= 1;
+        i += 1;
+    }
+
+    println!("{}", out.iter().rev().map(|x| (x + b'0') as char).collect::<String>());
 }
 
-fn bcd_write(out: &mut [u32], mut val: u32) {
-    for v in out {
-        *v = bcd_offset(&mut val);
+fn aes128_ecb_encrypt(input: &mut [u8; 16], key: &[u8]) {
+    let cipher = Cipher::aes_128_ecb();
+    let output = encrypt(cipher, key, None, input).unwrap();
+    for i in 0..16 {
+        input[i] = output[i];
     }
 }
 
-fn bcd_offset(val: &mut u32) -> u32 {
-    let mut out = *val % 10;
-    *val /= 10;
-    out |= (*val % 10) << 4;
-    *val /= 10;
+#[test]
+fn test_key_from_time() {
+    let serial = "356269991449";
+    let time = Utc.ymd(2020, 1, 10).and_hms(0, 4, 0);
+    let bcd_time = bcd_time(time);
 
-    out
+    let mut key0 = [0; 16];
+    key_from_time(bcd_time, 2, &serial, &mut key0);
+
+    assert_eq!(
+        key0,
+        [32, 32, 170, 170, 170, 170, 170, 170, 105, 153, 20, 73, 187, 187, 187, 187,]
+    );
 }
 
-fn bcd_time(time: DateTime<Utc>) -> [u32; 8] {
-    let mut bcd_time = [0; 8];
+fn key_from_time(bcd_time: [u8; 8], bcd_time_bytes: usize, serial: &str, key: &mut [u8; 16]) {
+    for i in 0..8 {
+        key[i] = 0xaa;
+    }
+    for i in 12..key.len() {
+        key[i] = 0xbb;
+    }
+    for i in 0..bcd_time_bytes {
+        key[i] = bcd_time[i];
+    }
 
-    bcd_write(&mut bcd_time[0..=1], time.year() as u32);
-    bcd_write(&mut bcd_time[2..3], time.month());
-    bcd_write(&mut bcd_time[3..4], time.day());
-    bcd_write(&mut bcd_time[4..5], time.hour());
-    bcd_write(&mut bcd_time[5..6], time.minute() & !0b11);
+    let serial: Vec<_> = serial.chars().map(|c| c.to_digit(10).unwrap() as u8).collect();
 
-    bcd_time
+    let mut k = 8;
+    let mut i = 4;
+    while i < 12 {
+        key[k] = (serial[i] << 4) | serial[i + 1];
+        k += 1;
+        i += 2;
+    }
 }
 
 #[test]
@@ -53,16 +112,29 @@ fn test_bcd_time() {
     assert_eq!(bcd_time, [32, 32, 1, 9, 35, 32, 0, 0]);
 }
 
+fn bcd_time(time: DateTime<Utc>) -> [u8; 8] {
+    let mut bcd_time = [0; 8];
 
-    // private static int[] key_from_time(int[] bcd_time, int bcd_time_bytes, String serial, int[] key) {
-    //     Arrays.fill(key, 0, 8, 0xaa);
-    //     Arrays.fill(key, 12, key.length, 0xbb);
-    //     System.arraycopy(bcd_time, 0, key, 0, bcd_time_bytes);
+    bcd_write(&mut bcd_time[0..=1], time.year() as u16);
+    bcd_write(&mut bcd_time[2..3], time.month() as u16);
+    bcd_write(&mut bcd_time[3..4], time.day() as u16);
+    bcd_write(&mut bcd_time[4..5], time.hour() as u16);
+    bcd_write(&mut bcd_time[5..6], (time.minute() & !0b11) as u16);
 
-    //     int k = 8;
-    //     for (int i = 4; i < 12; i += 2) {
-    //         key[k++] = ((serial.charAt(i) - '0') << 4) | (serial.charAt(i + 1) - '0');
-    //     }
+    bcd_time
+}
 
-    //     return key;
-    // }
+fn bcd_write(out: &mut [u8], mut val: u16) {
+    for v in out {
+        *v = bcd_offset(&mut val);
+    }
+}
+
+fn bcd_offset(val: &mut u16) -> u8 {
+    let mut out = (*val % 10) as u8;
+    *val /= 10;
+    out |= ((*val % 10) as u8) << 4;
+    *val /= 10;
+
+    out
+}
