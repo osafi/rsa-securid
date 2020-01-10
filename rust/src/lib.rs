b2fa254d40f24da6e495a3ac2e73ddc1c0cb0e2b
Alpha Chen <alpha@kejadlen.dev>
2020-01-10 12:30:50 -0800

    make more functions use Bytes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,156 @@
+use bytes::{BufMut, Bytes, BytesMut};
 use chrono::prelude::*;
 use openssl::symm::{encrypt, Cipher};
+use regex::Regex;
+use std::str::FromStr;
+
+pub struct Token {
+    pub seed: Seed,
+    pub serial: Serial,
+}
+
+impl Token {
+    pub fn code(&self, time: &DateTime<Utc>) -> String {
+        let bcd_time = Self::bcd_time(time);
+
+        let mut key0 = [0; 16];
+        let mut key1 = [0; 16];
+
+        Self::key_from_time(&bcd_time.slice(0..2), &self.serial.bytes, &mut key0);
+        aes128_ecb_encrypt(&mut key0, &self.seed.bytes);
+
+        Self::key_from_time(&bcd_time.slice(0..3), &self.serial.bytes, &mut key1);
+        aes128_ecb_encrypt(&mut key1, &key0);
+
+        Self::key_from_time(&bcd_time.slice(0..4), &self.serial.bytes, &mut key0);
+        aes128_ecb_encrypt(&mut key0, &key1);
+
+        Self::key_from_time(&bcd_time.slice(0..5), &self.serial.bytes, &mut key1);
+        aes128_ecb_encrypt(&mut key1, &key0);
+
+        Self::key_from_time(&bcd_time.slice(0..8), &self.serial.bytes, &mut key0);
+        aes128_ecb_encrypt(&mut key0, &key1);
+
+        let mut i = ((time.minute() as usize) & 0b11) << 2;
+        let t1 = ((key0[i + 0] as u32) & 0xff) << 24;
+        let t2 = ((key0[i + 1] as u32) & 0xff) << 16;
+        let t3 = ((key0[i + 2] as u32) & 0xff) << 8;
+        let t4 = ((key0[i + 3] as u32) & 0xff) << 0;
+        let mut token_code = t1 | t2 | t3 | t4;
+
+        let mut out = vec![];
+        let mut j: isize = ((0b0100001111011001 & FLD_DIGIT_MASK) >> FLD_DIGIT_SHIFT) as isize;
+        while j >= 0 {
+            let mut c = (token_code % 10) as u8;
+            token_code /= 10;
+
+            if i < 8 {
+                c += 0;
+            }
+            out.push(c % 10);
+
+            j -= 1;
+            i += 1;
+        }
+
+        out.iter().rev().map(u8::to_string).collect()
+    }
+
+    fn aes128_ecb_encrypt(input: &mut [u8; 16], key: &[u8]) {
+        let cipher = Cipher::aes_128_ecb();
+        let output = encrypt(cipher, key, None, input).unwrap();
+        for i in 0..16 {
+            input[i] = output[i];
+        }
+    }
+
+    fn key_from_time(bcd_time_slice: &Bytes, serial: &Bytes, key: &mut [u8; 16]) {
+        for i in 0..8 {
+            key[i] = 0xaa;
+        }
+        for i in 12..key.len() {
+            key[i] = 0xbb;
+        }
+        for (i, val) in bcd_time_slice.iter().enumerate() {
+            key[i] = *val;
+        }
+
+        let mut k = 8;
+        let mut i = 4;
+        while i < 12 {
+            key[k] = (serial[i] << 4) | serial[i + 1];
+            k += 1;
+            i += 2;
+        }
+    }
+
+    fn bcd_time(time: &DateTime<Utc>) -> Bytes {
+        let mut bytes = BytesMut::with_capacity(8);
+
+        Self::bcd_write(&mut bytes, (time.year() % 100) as u8);
+        Self::bcd_write(&mut bytes, (time.year() / 100 % 100) as u8);
+        Self::bcd_write(&mut bytes, time.month() as u8);
+        Self::bcd_write(&mut bytes, time.day() as u8);
+        Self::bcd_write(&mut bytes, time.hour() as u8);
+        Self::bcd_write(&mut bytes, (time.minute() & !0b11) as u8);
+        bytes.put_u8(0);
+        bytes.put_u8(0);
+
+        bytes.freeze()
+    }
+
+    fn bcd_write(bytes: &mut BytesMut, val: u8) {
+        let mut out = val % 10;
+        out |= (val / 10 % 10) << 4;
+        bytes.put_u8(out);
+    }
+}
+
+pub struct Seed {
+    bytes: Bytes,
+}
+
+impl FromStr for Seed {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"^(?:[[:xdigit:]]{2}:){15}[[:xdigit:]]{2}$").unwrap();
+        if !re.is_match(&s) {
+            return Err("the seed needs to be 16 octets separated by ':'".into());
+        }
+
+        let bytes = s
+            .split(':')
+            .map(|x| u8::from_str_radix(x, 16).unwrap())
+            .collect::<Vec<_>>()
+            .into();
+
+        Ok(Seed { bytes })
+    }
+}
+
+pub struct Serial {
+    bytes: Bytes,
+}
+
+impl FromStr for Serial {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"^\d{12}$").unwrap();
+        if !re.is_match(&s) {
+            return Err("the serial needs to be 12 digits".into());
+        }
+
+        let bytes = s
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect::<Vec<_>>()
+            .into();
+
+        Ok(Serial { bytes })
+    }
+}
 
 const FLD_DIGIT_SHIFT: u8 = 6;
 const FLD_DIGIT_MASK: u16 = (0b111 << FLD_DIGIT_SHIFT);
@@ -109,25 +260,18 @@ fn bcd_offset(val: &mut u32) -> u8 {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use test::Bencher;
 
     #[test]
     fn test_bcd_time() {
         let time = Utc.ymd(2020, 1, 9).and_hms(23, 23, 0);
-        let bcd_time = bcd_time(time);
+        let bcd_time = Token::bcd_time(&time);
 
-        assert_eq!(bcd_time, [32, 32, 1, 9, 35, 32, 0, 0]);
-    }
-
-    #[bench]
-    fn bench_bcd_time(b: &mut Bencher) {
-        let time = Utc.ymd(2020, 1, 9).and_hms(23, 23, 0);
-        b.iter(|| bcd_time(time));
+        assert_eq!(bcd_time, Bytes::from_static(&[32, 32, 1, 9, 35, 32, 0, 0]));
     }
 
     #[test]
     fn test_key_from_time() {
-        let serial = "356269991449";
+        let serial = [3, 5, 6, 2, 6, 9, 9, 9, 1, 4, 4, 9];
         let time = Utc.ymd(2020, 1, 10).and_hms(0, 4, 0);
         let bcd_time = bcd_time(time);
 
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,22 +1,61 @@
-extern crate chrono;
-extern crate openssl;
+#[macro_use]
+extern crate clap;
 
-use std::env;
-use rsa_securid::code;
+use chrono::prelude::*;
+use clap::{App, Arg};
+use rsa_securid::{Seed, Serial, Token};
 
 fn main() {
-    let serial: Vec<_> = env::var("RSA_AUTH_SERIAL")
-        .unwrap()
-        .chars()
-        .map(|c| c.to_digit(10).unwrap() as u8)
-        .collect();
+    let matches = App::new("RSA SecurID Authenticate")
+        .version(crate_version!())
+        .arg(
+            Arg::with_name("seed")
+                .long("seed")
+                .value_name("SEED")
+                .help("Device seed")
+                .required(true)
+                .require_equals(true),
+        )
+        .arg(
+            Arg::with_name("serial")
+                .long("serial")
+                .value_name("SERIAL")
+                .help("RSA token serial number")
+                .required(true)
+                .require_equals(true),
+        )
+        .arg(
+            Arg::with_name("pin")
+                .long("pin")
+                .value_name("PIN")
+                .help("Token PIN")
+                .require_equals(true)
+                .default_value("00000000"),
+        )
+        .arg(
+            Arg::with_name("flags")
+                .long("flags")
+                .value_name("FLAGS")
+                .help("Internal flag for token generation algorithm")
+                .require_equals(true)
+                .default_value("0100001111011001"),
+        )
+        .arg(
+            Arg::with_name("length")
+                .long("length")
+                .value_name("LENGTH")
+                .help("Length of the returned code")
+                .require_equals(true)
+                .takes_value(true)
+                .default_value("8"),
+        )
+        .get_matches();
 
-    let seed: Vec<u8> = env::var("RSA_AUTH_SEED")
-        .unwrap()
-        .split(':')
-        .map(|x| u8::from_str_radix(x, 16).unwrap())
-        .collect();
+    let seed = value_t!(matches, "seed", Seed).unwrap_or_else(|e| e.exit());
+    let serial = value_t!(matches, "serial", Serial).unwrap_or_else(|e| e.exit());
 
-    println!("{}", code(&serial, &seed));
+    let tokens = Token { seed, serial };
+
+    let time = Utc::now();
+    println!("{}", tokens.code(&time));
 }
-
