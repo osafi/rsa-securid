5a021c9df98188aa8fc71231ff7213f6afe63576
Alpha Chen <alpha@kejadlen.dev>
2020-05-16 16:28:03 -0700

    refactor Swift implementation

--- swift/RSA.playground/Contents.swift
+++ swift/RSA.playground/Contents.swift
@@ -11,7 +11,13 @@ extension Int {
             d.append(UInt8((hundreds << 4) | tens))
             n = q
         }
-        return d.isEmpty ? [0] : d.reversed()
+        return Data(d.isEmpty ? [0] : d.reversed())
+    }
+}
+
+extension UInt32 {
+    var digits: [UInt8] {
+        String(self).map { UInt8($0.wholeNumberValue!) }
     }
 }
 
@@ -21,100 +27,14 @@ let fldNumsecondsShift: UInt16 = 0
 let fldNumsecondsMask: UInt16 = 0b11 << fldNumsecondsShift
 
 class Token {
-    let serial: [UInt8]
-    let seed: [UInt8]
-    let pin: [UInt8] = Array(repeating: 0, count: 8)
+    let serial: Data
+    let seed: Data
+    let pin = Data(count: 8)
     let flags: UInt = 17369
+
     let currentDate: () -> Date
 
-    init?(serial: String, seed: String, currentDate: @escaping () -> Date = { Date() }) {
-        guard let serial = Int(serial)?.bcd else {
-            return nil
-        }
-        self.serial = serial
-
-        var d: [UInt8] = []
-        for hex in seed.components(separatedBy: ":").map({ UInt8($0, radix: 16) }) {
-            guard let hex = hex else { return nil }
-            d.append(hex)
-        }
-        self.seed = d
-
-        self.currentDate = currentDate
-    }
-
-    var code: [UInt8] {
-        let date = dateComponents
-        let bcdTime = self.bcdTime(date: date)
-
-        var key1 = seed
-        var key0 = key(from: Array(bcdTime[0..<2]))
-        key0 = encrypt(data: key0, with: key1)
-
-        key1 = key(from: Array(bcdTime[0..<3]))
-        key1 = encrypt(data: key1, with: key0)
-
-        key0 = key(from: Array(bcdTime[0..<4]))
-        key0 = encrypt(data: key0, with: key1)
-
-        key1 = key(from: Array(bcdTime[0..<5]))
-        key1 = encrypt(data: key1, with: key0)
-
-        key0 = key(from: Array(bcdTime[0..<8]))
-        key0 = encrypt(data: key0, with: key1)
-
-        let offset: Int
-        switch interval {
-        case .thirtySeconds:
-            offset = {
-                var i = 0
-                if date.minute! % 2 == 1 {
-                    i |= 0b1000
-                }
-                if date.minute! >= 30 {
-                    i |= 0b0100
-                }
-                return i
-            }()
-        case .oneMinute:
-            offset = (date.minute! & 0b11) << 2
-        }
-
-        let len = (UInt16(flags) & fldDigitMask) >> fldDigitShift
-
-        let raw: [Int] = {
-            String(
-                Data(key0[offset..<offset+4])
-                    .withUnsafeBytes { $0.load(as: UInt32.self) }
-                    .bigEndian
-                ).map { $0.wholeNumberValue! }.suffix(Int(len + 1))
-        }()
-
-        return zip(
-            raw,
-            (0..<raw.count).map { Int($0 < pin.count ? pin[$0] : 0) }
-        ).map { UInt8($0 + $1) }
-    }
-
-    var dateComponents: DateComponents {
-        Calendar.current.dateComponents(
-            in: TimeZone(identifier: "UTC")!,
-            from: currentDate()
-        )
-    }
-
-    private func bcdTime(date: DateComponents) -> [UInt8] {
-        return [
-            (date.year! % 100).bcd,
-            ((date.year! / 100) % 100).bcd,
-            date.month!.bcd,
-            date.day!.bcd,
-            date.hour!.bcd,
-            (date.minute! & Int(minuteMask)).bcd,
-            [0],
-            [0],
-        ].flatMap { $0 }
-    }
+    private var numDigits: Int { Int((UInt16(flags) & fldDigitMask) >> fldDigitShift + 1) }
 
     private var minuteMask: UInt8 {
         switch interval {
@@ -132,7 +52,64 @@ class Token {
         }
     }
 
-    func key(from bcdTime: [UInt8]) -> [UInt8] {
+    init(serial: Data, seed: Data, currentDate: @escaping () -> Date = { Date() }) {
+        self.serial = serial
+        self.seed = seed
+
+        self.currentDate = currentDate
+    }
+
+    var code: [UInt8] {
+        let date = Calendar.current.dateComponents(
+            in: TimeZone(identifier: "UTC")!,
+            from: currentDate()
+        )
+        let bcdTime = self.bcdTime(date: date)
+
+        var key1 = seed
+        var key0 = key(from: bcdTime[0..<2])
+        key0 = encrypt(data: key0, with: key1)
+
+        key1 = key(from: bcdTime[0..<3])
+        key1 = encrypt(data: key1, with: key0)
+
+        key0 = key(from: bcdTime[0..<4])
+        key0 = encrypt(data: key0, with: key1)
+
+        key1 = key(from: bcdTime[0..<5])
+        key1 = encrypt(data: key1, with: key0)
+
+        key0 = key(from: bcdTime[0..<8])
+        key0 = encrypt(data: key0, with: key1)
+
+        let offset = self.offset(minute: date.minute!)
+        let rawNumber = Data(key0[offset..<offset+4])
+            .withUnsafeBytes { $0.load(as: UInt32.self) }
+            .bigEndian
+
+        let pin = (0..<numDigits).map { $0 < self.pin.count ? self.pin[$0] : 0 }
+        return zip(rawNumber.digits.suffix(numDigits), pin).map { UInt8($0 + $1) }
+    }
+
+    private func bcdTime(date: DateComponents) -> Data {
+        var data = Data()
+
+        for component in [
+            date.year! % 100,
+            (date.year! / 100) % 100,
+            date.month!,
+            date.day!,
+            date.hour!,
+            date.minute! & Int(minuteMask),
+        ] {
+            data.append(component.bcd)
+        }
+        data.append(contentsOf: [0, 0])
+
+        return data
+    }
+
+    private func key(from bcdTime: Data) -> Data {
         var key: [UInt8] = Array(repeating: 0, count: 16)
 
         for i in 0..<16 {
@@ -150,12 +127,10 @@ class Token {
             key[i] = byte
         }
 
-        return key
+        return Data(key)
     }
 
-    func encrypt(data: [UInt8], with key: [UInt8]) -> [UInt8] {
-        let key = Data(key)
-        let data = Data(data)
+    private func encrypt(data: Data, with key: Data) -> Data {
         var encrypted = Data(count: data.count)
 
         encrypted.withUnsafeMutableBytes { encrypted in
@@ -179,13 +154,33 @@ class Token {
             }
         }
 
-        return Array(encrypted)
+        return encrypted
+    }
+
+    private func offset(minute: Int) -> Int {
+        switch interval {
+        case .thirtySeconds:
+            var i = 0
+            if minute % 2 == 1 { i |= 0b1000 }
+            if minute >= 30 { i |= 0b0100 }
+            return i
+        case .oneMinute:
+            return (minute & 0b11) << 2
+        }
     }
 
 }
 
+let seed: Data = {
+    Data(
+        ""
+            .components(separatedBy: ":")
+            .map { UInt8($0, radix: 16)! }
+    )
+}()
+
 let token = Token(
-    serial: "",
-    seed: ""
-)!
+    serial: 0.bcd,
+    seed: seed
+)
 print(token.code.map { String($0) }.joined())
